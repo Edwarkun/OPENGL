27d4028223ed1a4fc6fd80c00a2475eaf1894b23
Edwarkun <edwarkun@gmail.com>
2017-03-30 23:31:13 +0200

    Multiple texture usage functional!

--- Practica1/src/main.cpp
+++ Practica1/src/main.cpp
@@ -4,6 +4,7 @@
 //GLFW
 #include <GLFW\glfw3.h>
 #include <iostream>
+#include <SOIL.h>
 //Shader
 #include "shader.h"
 
@@ -12,6 +13,7 @@ const GLint WIDTH = 800, HEIGHT = 600;
 bool WIREFRAME = false;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void LoadTexture(GLuint&, int&, int&, const std::string&);
 
 int main() {
 	/////////////////// GLFW INITIALISATION ////////////////////
@@ -49,21 +51,22 @@ int main() {
 		exit(EXIT_FAILURE);
 	}
 	/////////////////// SHADER LOADING ////////////////////
-	Shader shader("./src/SimpleVertexShader.vertexshader", "./src/SimpleFragmentShader.fragmentshader");
+	Shader shader("./src/TextureVertexShader.vertexshader", "./src/TextureFragmentShader.fragmentshader");
 
 	//Vertices Definition
 	//Here we initialize the points that will form our shape
-	float vertices []{
-		-0.2f, -0.2f, 0.f,
-		0.2f, -0.2f, 0.f,
-		0.2f, 0.2f, 0.f,
-		-0.2f, 0.2f, 0.f
+	GLfloat vertices[] = {
+		// Positions          // Colors           // Texture Coords
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f, // Top Right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   0.0f, 1.0f, // Bottom Right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f, // Bottom Left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   1.0f, 0.0f  // Top Left 
 	};
 	//EBO definition
 	//Here we define which point form a triangle
 	int triangles[]{
-		0, 1, 3,
-		1, 2, 3
+		0, 3, 1,
+		2, 1, 3
 	};
 
 	// VBO, VAO and EBO creation
@@ -85,12 +88,32 @@ int main() {
 	/////////////////// VAO SETUP ////////////////////
 	glGenVertexArrays(1, &VAO);//We create a vertex array buffer that will store the VAO properties
 	glBindVertexArray(VAO); // We bind that VAO to our pointer
-	glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), (GLvoid*)0);//Here we store enough memory for a vao taht will work with 3 dimensional vertices
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, false, 8 * sizeof(float), (GLvoid*)0);//Here we store enough memory for a vao taht will work with 3 dimensional vertices
+	glEnableVertexAttribArray(0);//We bind that layout to the shader
+	
+	glVertexAttribPointer(1, 3, GL_FLOAT, false, 8 * sizeof(float), (GLvoid*)(3 * sizeof(GLfloat)));//Storage for the color coordinates
+	glEnableVertexAttribArray(1);//We bind that layout to the shader
+	
+	glVertexAttribPointer(2, 2, GL_FLOAT, false, 8 * sizeof(float), (GLvoid*)(6 * sizeof(GLfloat)));//Storage for the texture normals
+	glEnableVertexAttribArray(2);//We bind that layout to the shader
 
-	//We reset the bindings
-	glEnableVertexAttribArray(0);
+	/////////////////// BINDINGS RESET ////////////////////
+	
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	/////////////////// TEXTURE LOADING ////////////////////
+	//Texture 1
+	GLuint T1;
+	int T1W, T1H; // We will store the texture width and height in those variables
+	LoadTexture(T1, T1W, T1H, "./src/texture.png");
+
+	//Texture 1
+	GLuint T2;
+	int T2W, T2H; // We will store the texture width and height in those variables
+	LoadTexture(T2, T2W, T2H, "./src/planetTexture.png");
+
 
 	//DRAW LOOP
 	while (!glfwWindowShouldClose(window)) {
@@ -102,14 +125,28 @@ int main() {
 		/////////////////// SHADER USAGE ////////////////////
 		shader.USE();
 
-		/////////////////// UNIFORM VARIABLES ////////////////////
-		GLint offset = glGetUniformLocation(shader.Program, "offset"); // We save the direction of the variable offset to a pointer
-		glUniform1f(offset, abs(sin(glfwGetTime()) * 0.2f));
+		/////////////////// UNIFORM VARIABLES TWEAKING ////////////////////
+		/*GLint offset = glGetUniformLocation(shader.Program, "offset"); // We save the direction of the variable offset to a pointer
+		glUniform1f(offset, abs(sin(glfwGetTime()) * 0.2f));*/
+		GLint texClamp = glGetUniformLocation(shader.Program, "textureClamp");
+		glUniform1f(texClamp, (sin(glfwGetTime()) + 1) / 2);
 
-		//pitar el VAO
+		/////////////////// BIND VAO AND EVO ////////////////////
 		glBindVertexArray(VAO); // We are using the vao attributes here, we "paint" the VAO
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // WE define the EBO we're using
 
+		/////////////////// TEXTURE USAGE ////////////////////
+		//We bind the first texture to the uniform variable "Texture1" in the fragment shader
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, T1);
+		glUniform1i(glGetUniformLocation(shader.Program, "Texture1"), 0);
+
+		//We do the same as before but using another buffer and the second texture
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, T2);
+		glUniform1i(glGetUniformLocation(shader.Program, "Texture2"), 1);
+
+		/////////////////// WIREFRAME / FILL & PAINT ////////////////////
 		if (!WIREFRAME) {
 			glPolygonMode(GL_FRONT_AND_BACK, GL_POLYGON);
 			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -118,15 +155,11 @@ int main() {
 			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		}
-
-		glBindVertexArray(0);
-
-		/////////////////// SWAP SCREEN BUFFERS /////////////////////
-		glfwSwapBuffers(window);
-
+		glBindVertexArray(0); // Reset VAO
 		/////////////////// INPUT PROCESSING ////////////////////
 		glfwPollEvents();
-
+		/////////////////// SWAP SCREEN BUFFERS /////////////////////
+		glfwSwapBuffers(window);
 	}
 
 	// Free the VAO, VBO and EBO
@@ -149,4 +182,18 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	if (key == GLFW_KEY_W && action == GLFW_PRESS) {
 		WIREFRAME = !WIREFRAME;
 	}
+}
+void LoadTexture(GLuint& pointer, int& width, int& height, const std::string& path) {
+	glGenTextures(1, &pointer); // We generate the actual texture
+	glBindTexture(GL_TEXTURE_2D, pointer); // WE bind it to the graphics card
+										   // Set the texture wrapping/filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Load and generate the texture
+	unsigned char* myTexture = SOIL_load_image(path.c_str(), &width, &height, 0, SOIL_LOAD_RGB);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, myTexture); // We generate the texture
+	SOIL_free_image_data(myTexture); // We free the image since we have stored it
+	glBindTexture(GL_TEXTURE_2D, 0); // We bind the texture to the graphics card
 }
